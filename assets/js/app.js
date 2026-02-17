ba3e504151cd0a3b5919b64e534a2e6a209d84d5
Stefan Wagner <stefan@wlanhome196209.uni-regensburg.de>
2026-02-17 07:50:18 +0100

    Use GitHub Secrets for API keys via config.js injection

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,6 @@
-const SCHEDULE_BIN_ID = "699332e2ae596e708f2f7434"; // Schedule
-const EMPLOYEES_BIN_ID = "699333dcd0ea881f40bf132f"; // Employees
-const API_KEY = "$2a$10$5f5WR8jrQAQp2TgNWGvWb.2tp/RA1ZzQzMv3SY5uwYnm5oqz66yxa"; // Master Key
+const SCHEDULE_BIN_ID = CONFIG.SCHEDULE_BIN_ID; // Schedule
+const EMPLOYEES_BIN_ID = CONFIG.EMPLOYEES_BIN_ID; // Employees
+const API_KEY = CONFIG.API_KEY; // Master Key
 
 let currentSchedule = [];
 let currentEmployees = [];
